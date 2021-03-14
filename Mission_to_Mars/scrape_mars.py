4d29318149c51f22e4f3daf152bedd76412a7b23
Hilary <hilary.koerner@gmail.com>
2021-03-14 09:58:53 -0500

    finish jupyter notebook, set up python files

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,27 +1,56 @@
-from splinter import Browser
-from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
 
-def intialize_browser()
-    #setup Splinter
-    executable_path ={'executable_path': ChromeDriverManager().install()}
-    return Browser("chrome", **executable_path, headless=False)
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+def initialize_browser():
+    # Setup splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape()
-    browser = intialize_browser()
+    browser = initialize_browser()
 
-    # listings = {}
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    browser.visit()
+    listings = {}
+
+    news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(news_url)
 
     html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    soup = bs(html, "html.parser")
 
-    # listings["headline"] = soup.find("a", class_="title").get_text()
-    # listings["headline"] = soup.find("a", class_="title").get_text()
-    # listings["headline"] = soup.find("a", class_="title").get_text()
+    top_news = soup.find("div", class_="list_text")
+    title = top_news.find("div", class_="content_title")
+    title = title.text.strip()
+    print(title)
 
-    #quit the browser
+
+    news_paragraph = top_news.find("div", class_="article_teaser_body")
+    print(news_paragraph.text.strip())
+
+
+    mars_facts_url = 'https://space-facts.com/mars/'
+
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    mars_facts_url = 'https://space-facts.com/mars/'
+    browser.visit(mars_facts_url)
+
+
+    tables = pd.read_html(mars_facts_url)
+    tables
+
+    type(tables)
+
+
+    mars_facts = tables[0]
+    mars_facts = mars_facts.rename(columns={0:"Data", 1:'Mars'})
+    mars_facts.set_index("Data")
+    mars_facts
+
+    #quit the broswer
     browser.quit()
 
-    # return listings
+    return listings
