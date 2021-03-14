16c05742c729817d914f7a48509bcc712c7d10e5
Hilary <hilary.koerner@gmail.com>
2021-03-14 14:17:57 -0500

    flask app, python

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,22 +1,26 @@
+
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+info_temp = {}
+
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+# mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    #info = mongo.db.info.find_one()
+    return render_template("index.html", info=info_temp)
 
 @app.route("/scrape")
 def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
+    #info = mongo.db.info
+    #info_data = scrape_mars.scrape()
+    #info.update({}, info_data, upsert=True)
+    info_temp = scrape_mars.scrape()
     return redirect("/", code=302)
 
 if __name__ == "__main__":
--- Mission_to_Mars/old_scrape_mars.py
+++ Mission_to_Mars/old_scrape_mars.py
@@ -4,17 +4,15 @@ from bs4 import BeautifulSoup as bs
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
-
 def initialize_browser():
     # Setup splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    return Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
 
-
-def scrape():
+def scrape()
     browser = initialize_browser()
 
-    info = {}
+    listings = {}
 
     news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     browser.visit(news_url)
@@ -24,35 +22,35 @@ def scrape():
 
     top_news = soup.find("div", class_="list_text")
     title = top_news.find("div", class_="content_title")
-    info['title'] = title.text.strip()
+    title = title.text.strip()
+    print(title)
 
-    info['news_paragraph'] = top_news.find("div", class_="article_teaser_body")
 
-    jpl_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
-    browser.visit(jpl_url)
+    news_paragraph = top_news.find("div", class_="article_teaser_body")
+    print(news_paragraph.text.strip())
 
-    jpl_html = browser.html
-    soup = bs(jpl_html, "html.parser")
-
-    top_image = soup.find("img", class_="headerimage fade-in")['src']
-
-    info['featured_image']  = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/'+top_image
 
     mars_facts_url = 'https://space-facts.com/mars/'
 
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    mars_facts_url = 'https://space-facts.com/mars/'
+    browser.visit(mars_facts_url)
+
+
     tables = pd.read_html(mars_facts_url)
+    tables
+
+    type(tables)
+
 
     mars_facts = tables[0]
     mars_facts = mars_facts.rename(columns={0:"Data", 1:'Mars'})
-    mars_facts = mars_facts.set_index("Data")
-    info['mars_facts_df'] = mars_facts
-    info['mars_facts_dict'] = mars_facts.to_dict()
+    mars_facts.set_index("Data")
+    mars_facts
 
     #quit the broswer
     browser.quit()
 
-    return info
-
-if __name__ == '__main__':
-    info = scrape()
-    print(info)
+    return listings
